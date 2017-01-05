a4f78e13baade375e50f6f440eb3d3ee505ff931
oswystan <oswystan@126.com>
2017-01-05 21:52:39 +0800

    feature: add config for database and other items

--- src/datastore/config.js
+++ src/datastore/config.js
@@ -3,18 +3,29 @@
  *                     Copyright (C) 2017 wystan
  *
  *       filename: config.js
- *    description: 
+ *    description:
  *        created: 2017-01-01 17:17:41
  *         author: wystan
  *
  *********************************************************************************
  */
 
+var fs = require("fs");
+
 module.exports = function() {
     this.dbname   = "contacts";
     this.user     = "pg_contacts";
     this.password = "123456";
     this.max_rows = 4;
+
+    var content = fs.readFileSync(__dirname + "/../../config/db.json", {encoding: "utf8"});
+    if (content) {
+        var c = JSON.parse(content);
+        this.dbname   = c.db;
+        this.user     = c.user;
+        this.password = c.password;
+        this.max_rows = c.max_rows;
+    }
 };
 
 

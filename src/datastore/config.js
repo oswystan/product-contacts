51bcc905b5e626d472842e44892e38a9672697b8
oswystan <oswystan@126.com>
2017-01-14 19:19:38 +0800

    refactor: remove config and move all stuff to src/datastore/config.js

--- src/datastore/config.js
+++ src/datastore/config.js
@@ -10,23 +10,11 @@
  *********************************************************************************
  */
 
-var fs = require("fs");
-
 module.exports = function() {
     this.dbname   = "contacts";
     this.user     = "pg_contacts";
     this.password = "123456";
     this.max_rows = 4;
-
-    var content = fs.readFileSync(__dirname + "/../../config/db.json", {encoding: "utf8"});
-    if (content) {
-        var c = JSON.parse(content);
-        this.dbname   = c.db;
-        this.user     = c.user;
-        this.password = c.password;
-        this.max_rows = c.max_rows;
-    }
 };
-
 
 /************************************* END **************************************/

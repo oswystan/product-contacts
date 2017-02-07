efed7652526e2181a0e05c4c22fd507e2e22c1de
oswystan <oswystan@126.com>
2017-02-07 17:41:16 +0800

    refactor: remove useless code

--- src/auth.js
+++ src/auth.js
@@ -14,13 +14,6 @@ var basic_auth = require('basic-auth-connect');
 var user_db = require('./config')().user;
 
 module.exports = function() {
-    /*
-    var user_db = [
-        {user: "guest", password: "guest",  role: 0},
-        {user: "admin", password: "admin0", role: 1}
-    ];
-    */
-
     return function (req, res, next) {
         function check(user, pass) {
             for (var i = 0, l = user_db.length; i < l; i++) {
